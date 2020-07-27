111c72be362414200c5bec37a0ce36377aa4b2de
Alexander <webisforce@gmail.com>
2020-07-27 16:39:28 +0300

    sadsad sadsad

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,83 +1,30 @@
+
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+    siteMetadata: {
+        title: "Quarkly gatsby project",
+        description: "",
+        author: "@Alexander",
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sharp",
+        "gatsby-plugin-styled-components",
+        {
+            resolve: "gatsby-plugin-manifest",
             options: {
-              maxWidth: 590,
+                name: "gatsby-starter-default",
+                short_name: "starter",
+                start_url: "/",
+                background_color: "#663399",
+                theme_color: "#663399",
+                display: "minimal-ui",
+//              icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
             },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+        },
+        "gatsby-plugin-resolve-src",
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // "gatsby-plugin-offline",
+    ],
+};
---
+++ qapi.js
@@ -0,0 +1,24 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f0c23db46cf7b001ed1de45",
+				"5f0c23db46cf7b001ed1de47"
+			]
+		},
+		"5f0c23db46cf7b001ed1de45": {
+			"id": "5f0c23db46cf7b001ed1de45",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f0c23db46cf7b001ed1de47": {
+			"id": "5f0c23db46cf7b001ed1de47",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"projectType": "gatsby"
+}
